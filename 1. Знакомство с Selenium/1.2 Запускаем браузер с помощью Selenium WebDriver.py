39272a0f88e831f8acbedfe8b5961cfbc36bd808
Rustam Islakaev <rislakaev@gmail.com>
2026-05-10 19:55:33 +0300

    Done 1.2

--- 1. Знакомство с Selenium/1.2 Запускаем браузер с помощью Selenium WebDriver.py
+++ 1. Знакомство с Selenium/1.2 Запускаем браузер с помощью Selenium WebDriver.py
@@ -1,2 +1,19 @@
-a = "b"
-print(a)
+# Запуск окружения python3 -m venv selenium_env
+# source selenium_env/bin/activate
+# Декативируем deactivate
+# Запуск интерпритатора python3
+# Когда открываете новое окно терминала, нужно заново активировать виртуальное окружение.
+
+# Задача 1
+import math
+fun = lambda x : 1 if x == 1 else math.ceil(math.sinh(fun (x-1)))
+print(fun(5))
+#723128532146
+
+# Задача 2
+# driver.get()
+
+
+
+
+
