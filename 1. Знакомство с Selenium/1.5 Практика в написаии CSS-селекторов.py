d450457bfe22a6870079611bfd9b5b94d5228872
Rustam Islakaev <rislakaev@gmail.com>
2026-05-12 11:21:25 +0300

    Done 1.5 > Last Lesson 1.6

---
+++ 1. Знакомство с Selenium/1.5 Практика в написаии CSS-селекторов.py
@@ -0,0 +1,22 @@
+#ID-селекторы
+#Задача 1
+# #polite
+
+#Задача 2
+#p[data-type="description"]
+
+#Задача 3
+#p[class="text"]
+
+#Задача 4
+#div[class="card-body watermelon"]
+#p[class= "description"]
+
+#Задача 5
+#:nth-child(2)>
+#p[data-type="description"]
+
+#Задача 6
+#:nth-child(2)>
+#div[class="btn-group"]
+#:nth-child(2)
---
+++ 1. Знакомство с Selenium/1.6 Поиск элементов с помощью Selenium WebDriver.py
@@ -0,0 +1,40 @@
+#Для поиска Selenium предоставляет метод find_element, который принимает два аргумента - тип локатора и значение локатора
+
+#find_element(By.ID, value) - поиск по уникальному атрибуту id элемента.
+
+#find_element(By.CSS_SELECTOR, value) — поиск элемента с помощью правил на основе CSS
+
+#find_element(By.XPATH, value) - поиск с помощью языка запросов XPath
+
+#find_element(By.NAME, value) — поиск по атрибуту name элемента
+
+#find_element(By.TAG_NAME, value) — поиск элемента по названию тега элемента
+
+#find_element(By.CLASS_NAME, value) — поиск по значению атрибута class
+
+#find_element(By.LINK_TEXT, value) — поиск ссылки на странице по полному совпадению
+
+#find_element(By.PARTIAL_LINK_TEXT, value) — поиск ссылки на странице, если текст селектора совпадает с любой частью текста ссылки.
+
+
+#browser.close() закрывает текущее окно браузер
+#browser.quit() закрывает все окна
+
+
+
+#Задача 1
+# Ответ в файле lesson1.6.1.py
+
+#Задача 2
+# Ответ в файле lesson1.6.2
+
+#Задача 3
+# Ответ в файле lesson1.6.3
+
+#Задача 4
+# Ответ в файле lesson1.6.4
+
+
+#assert "Congratulations! You have successfully registered!" == welcome_text
+# с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+
